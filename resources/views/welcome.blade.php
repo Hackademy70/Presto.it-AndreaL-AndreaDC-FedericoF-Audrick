fb076d51734f7155abc206dfcdf475268406186c
andrea di cosimo <andrea.dicosimo1927@gmail.com>
2023-05-09 14:52:50 +0200

    aggiunto component e root

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,12 @@
 <x-layout>
     <h1 class="bg-primary vh-100">Hello world</h1>
+    <x-masthead />
+
+   
+    {{-- sezione card --}}
+   <div class="container">
+    <div class="row">
+        <x-card/>
+    </div>
+   </div>
 </x-layout>
