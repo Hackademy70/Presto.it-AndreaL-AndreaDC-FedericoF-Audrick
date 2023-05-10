3c19d79cb55a8fecd8b2aab8aab95b4cfc2bd75b
andrea di cosimo <andrea.dicosimo1927@gmail.com>
2023-05-10 16:09:51 +0200

    aggiunto database categorie

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +15,21 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+           'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password'=>bcrypt('password'),
+        ]);
+        
+        $categories = [
+            'moda',
+            'casa',
+            'auto',
+            'animali',
+            'eletronica',
+        ];
+        foreach ($categories as $category){
+         Category::create(['name'=>$category]);   
+        }
     }
 }
