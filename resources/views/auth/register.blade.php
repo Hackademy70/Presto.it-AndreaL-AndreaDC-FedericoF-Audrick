cd89e22f4ea80a16bcbdf5f4b6cdfe6cc6be89a0
andrea di cosimo <andrea.dicosimo1927@gmail.com>
2023-05-11 11:06:58 +0200

    regex

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,7 +12,7 @@
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputEmail1" class="form-label">email address</label>
-                        <input name="email" type="email" class="form-control"
+                        <input name="email"  title="dio can impara a leggere" pattern="[\w\d]{3,30}@[\w\d]{2,7}.[\w]{2,5}" class="form-control"
                         id="exampleInputEmail1" aria-describedby="emailHelp">
                         <div id="emailHelp" class="form-text">we ll never share your email whit
                             annyone else.
@@ -20,10 +20,11 @@
                     </div>
                     <div class="mb-3">
                         <label for="exampleInputPassword1" class="form-label">password</label>
-                        <input name="password" type="password" class="form-control" id="exampleInputPassword1">
+                        <input name="password" title="dio can impara a leggere" pattern="[A-z\d]{7,20}[@#$?!]{1,20}" type="password" class="form-control" id="exampleInputPassword1">
+                        
                     </div>
                     <div class="mb-3">
-                        <label for="password_confirmation" class="form-label">password</label>
+                        <label for="password_confirmation" class="form-label">conferma password</label>
                         <input name="password_confirmation" type="password" class="form-control" id="password_confirmation">
                     </div>
                     <button type="submit" class="btn btn-primary">registrati</button>
