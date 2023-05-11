a5287d5f60659693ca15ddfe30d4266fee910ac0
audrick <kamtatchoffo@gmail.com>
2023-05-11 17:50:59 +0200

    nuova navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -49,78 +49,73 @@
          </div>
      </div>
  </nav> --}}
-
- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="navContain container-fluid mx-5 d-flex">
-        <div>
-            <a class="navbar-brand" href="{{ route('home') }}">Home</a>
-        </div>
-
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-            aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            
-            <form class="d-flex ms-auto my-3 mx-4">
-                <input class=" form-control  me-2" type="search" placeholder="Search" aria-label="Search" >
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-
-            
-            
-        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+ <section id="header">
+    <a href="{{ route('home') }}"><img src="{{Storage::url('img/logo.png')}}" class="logo" alt="logo"></a>
+    
+    <div>
+        <ul id="navbar">
             @guest
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+            <li class="container-fluid">
+                <form class="d-flex">
+                  <input class="inputx form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
             </li>
-            <li class="nav-item">
-                <a class="nav-link active,kylilu
-                " aria-current="page" href="{{ route('chisiamo') }}">Chi siamo</a>
-            </li>
-            
-            <li class="nav-but ">
-                <a class="btn btn-outline-light" aria-current="page" href="{{ route('register') }}">Registrati</a>
-            </li>
-        
-           
-            <li class="nav-item">
-                <a class="btn btn-outline-light" href="{{ route('login') }}">Accedi</a>
-            </li>
+            <li><a class="text-white" href="{{ route('home') }}">Home</a></li>
+            <!-- <li><a href="shop.html">articoli</a></li>            -->
+            <li><a class="text-white" href="{{ route('chisiamo') }}">Chisiamo</a></li>
+            <li><a class="btn btn-outline-light" href="{{ route('register') }}">registrati</a></li>
+            <!-- <li id ="lg-bag"><a href="cart.html"><i class="far fa-shopping-cart"></i></a></li>-->
+            <a href="#" id ="close"><i class="far fa-times"></i></a>
+            <li><a class="btn btn-outline-light" href="{{ route('login') }}">login</a></li>
             @else
-            
-            <li class="nav-item">
-                <a class="nav-link " href="{{ route('announcement.create') }}">crea annuncio</a>
-            </li>
-            
-            <li class="nav-item mx-2">
-                <a class="btn btn-outline-light dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                    data-bs-toggle="dropdown" aria-expanded="false">
-                    {{ Auth::user()->name }}
-                </a>
-                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#">Action</a></li>
-                    {{-- <li><a class="dropdown-item" href="{{ route('announcement.create') }}">crea annuncio</a></li> --}}
-                    <li>
-                        <hr class="dropdown-divider">
-                    </li>
-                    
-                    <form id="form-logout" action="{{ route('logout') }}" method="post" class="d-none">
+            <a href="#" id ="close"><i class="far fa-times"></i></a>
+                
+                <li><a class=" btn btn-outline-light" href="about.html">i miei annunci</a></li>
+                <li><a class=" btn btn-outline-light" href="{{ route('announcement.create') }}">crea annunci</a></li>
+                <li><a class=" btn btn-outline-light" href="about.html">{{ Auth::user()->name }}</a></li>
+                
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="https://mdbootstrap.com/img/Photos/Avatars/img (31).jpg" class="rounded-circle" height="22"
+                        alt="" loading="lazy" />
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      {{-- <li><a class="dropdown-item" href="#">Action</a></li>
+                      <li><a class="dropdown-item" href="#">Another action</a></li> --}}
+                      <form id="form-logout" action="{{ route('logout') }}" method="post" class="d-none">
                         @csrf
-                    </form>
-                </ul>
+                      </form>
+                      <li class="nav-item"><a class="btn btn-outline-dark" href="/logout"
+                        onclick="event.preventDefault();
+           getElementById('form-logout').submit()">logout</a>
+                    </li>
+                      {{-- <li><hr class="dropdown-divider"></li>
+                      <li><a class="dropdown-item" href="#">Something else here</a></li> --}}
+                    </ul>
+                  </li>
+                  @endguest
 
-            </li>
-            <li class="nav-item"><a class="btn btn-outline-light" href="/logout"
-                onclick="event.preventDefault();
-   getElementById('form-logout').submit()">logout</a>
-            </li>
-            
-            @endguest
         </ul>
-
-
-        </div>
     </div>
-</nav>
+    <div id ="mobile">
+        <a href=""><i class="far fa-shopping-cart"></i></a>
+        <i id="bar" class="fas fa-outdent"></i>
+    </div>
+</section>
+<script>
+    const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+if(bar){
+    bar.addEventListener('click', ()=>{
+        nav.classList.add('active') ; 
+    })
+}
+
+if(close){
+    close.addEventListener('click', ()=>{
+        nav.classList.remove('active') ; 
+    })
+}
+</script>
