01bce195350743d363b93a318e5f3b1764f00abe
Federico Ferrante <federico.ferrante98@gmail.com>
2023-05-11 17:35:16 +0200

    perfezionata home

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,13 +1,15 @@
 <div class="col-12 col-md-4 my-4">
   <div class="custom-card shadow">
-    <img src="https://picsum.photos/200" class="card-img-top p-3 rounded" alt="">
+    <img src="https://picsum.photos/200" class="card-img-top rounded" alt="">
     <div class="card-body">
       <h5 class="card-title">{{$announcement->title}}</h5>
       <p class="card-text">{{$announcement->body}}</p>
-      <p class="card-text">{{$announcement->price}}</p>
-      <a href="{{route('announcement.show', ['announcement'=>$announcement])}}" class="btn btn-primary">visualizza</a>
-      <a href="" class="my-2 pt-2 card-link btn btn-success">categoria: {{$announcement->category->name}}</a>
-      <p class="card-footer">publicato il: {{$announcement->created_at->format('d/m/y')}}</p>
+      <p class="card-text price">{{$announcement->price}}</p>
+      <div class="button-group">
+        <a href="{{route('announcement.show', ['announcement'=>$announcement])}}" class="btn btn-primary d-block w-100 mb-2">Visualizza</a>
+        <a href="" class="card-link btn btn-success d-block w-100">Categoria: {{$announcement->category->name}}</a>
+      </div>
+      <p class="card-footer">Pubblicato il: {{$announcement->created_at->format('d/m/y')}}</p>
     </div>
   </div>
 </div>
--- resources/views/components/masthead.blade.php
+++ resources/views/components/masthead.blade.php
@@ -1,4 +1,4 @@
-<section class="highlight-section">
+<section class="highlight-section mb-4">
   <div class="container">
     <h2 class="highlight-title">Benvenuto su Presto.it</h2>
     <p class="highlight-description">Scopri una vasta selezione di annunci per abbigliamento, accessori e altro ancora. Trova le migliori offerte e vendi ciò che non usi più.</p>
