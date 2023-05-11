90dc0bcd2e813e8f49ea0efe5e8dd54629d4672b
audrick <kamtatchoffo@gmail.com>
2023-05-11 10:24:55 +0200

    aggiunta frontend

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,32 +1,32 @@
 <x-layout>
     <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h1 class="text-center"> registrati!</h1>
+        <div class="row d-flex justify-content-center">
+            <div class="col-12 col-lg-4 my-3 ">
+                <h1 class="text-center text-success"> registrati!</h1>
                 <form action="{{route('register')}}" method="POST">
                     @csrf
                     <div class="mb-3">
-                        <label for="name" class="form-label">nome completo</label>
+                        <label for="name" class="form-label">Nome completo</label>
                         <input name="name" type="text" class="form-control" id="name"
                         aria-describedby="name">
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">email address</label>
+                        <label for="exampleInputEmail1" class="form-label">Email address</label>
                         <input name="email" type="email" class="form-control"
                         id="exampleInputEmail1" aria-describedby="emailHelp">
-                        <div id="emailHelp" class="form-text">we ll never share your email whit
+                        <div id="emailHelp" class="form-text">we will never share your email whit
                             annyone else.
                         </div>
                     </div>
                     <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">password</label>
+                        <label for="exampleInputPassword1" class="form-label">Password</label>
                         <input name="password" type="password" class="form-control" id="exampleInputPassword1">
                     </div>
                     <div class="mb-3">
-                        <label for="password_confirmation" class="form-label">password</label>
+                        <label for="password_confirmation" class="form-label">Password</label>
                         <input name="password_confirmation" type="password" class="form-control" id="password_confirmation">
                     </div>
-                    <button type="submit" class="btn btn-primary">registrati</button>
+                    <button type="submit" class="btn btn-dark container">Registrati</button>
                 </form>
             </div>
         </div>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,49 +1,126 @@
- <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="{{ route('home') }}">Navbar</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+ {{-- <nav class="navbar navbar-expand-lg bg-body-tertiary">
+     <div class="container-fluid">
+         <a class="navbar-brand" href="{{ route('home') }}">Navbar</a>
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+             <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarSupportedContent">
+             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                 <li class="nav-item">
+                     <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+                 </li>
+                 @guest
+                     <li class="nav-item">
+                         <a class="nav-link" href="{{ route('register') }}">registrati</a>
+                     </li>
+                     <li class="nav-item">
+                         <a class="nav-link" href="{{ route('login') }}">login</a>
+                     </li>
+                 @else
+                     <li class="nav-item dropdown">
+                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                             data-bs-toggle="dropdown" aria-expanded="false">
+                             {{ Auth::user()->name }}
+                         </a>
+                         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                             <li><a class="dropdown-item" href="#">Action</a></li>
+                             <li><a class="dropdown-item" href="{{ route('announcement.create') }}">crea annuncio</a></li>
+                             <li>
+                                 <hr class="dropdown-divider">
+                             </li>
+                             <li><a class="dropdown-item" href="/logout"
+                                     onclick="event.preventDefault();
+                        getElementById('form-logout').submit()">logout</a>
+                             </li>
+                             <form id="form-logout" action="{{ route('logout') }}" method="post" class="d-none">
+                                 @csrf
+                             </form>
+                         </ul>
+
+                     </li>
+                 @endguest
+               
+             </ul>
+             <form class="d-flex" role="search">
+                 <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                 <button class="btn btn-outline-success" type="submit">Search</button>
+             </form>
+         </div>
+     </div>
+ </nav> --}}
+
+ <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="navContain container-fluid mx-5 d-flex">
+        <div>
+            <a class="navbar-brand" href="{{ route('home') }}">Navbar</a>
+        </div>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+            aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
-                </li>
-                @guest
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('register')}}">registrati</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('login')}}">login</a>
-                </li>
-                @else
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" 
-                    role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      {{Auth::user()->name}}
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="{{route('announcement.create')}}">crea annuncio</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="/logout" onclick="event.preventDefault();
-                        getElementById('form-logout').submit()">logout</a></li>
-                        <form id="form-logout" action="{{route('logout')}}"
-                        method="post" class="d-none">
-                        @csrf
-                    </form>
-                    </ul>
-                </li>
-                @endguest
-                <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
-                </li>
-            </ul>
-            <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            
+            <form class="d-flex ms-auto my-3 mx-4">
+                <input class=" form-control  me-2" type="search" placeholder="Search" aria-label="Search" >
                 <button class="btn btn-outline-success" type="submit">Search</button>
             </form>
+
+            
+            
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            @guest
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active,kylilu
+                " aria-current="page" href="{{ route('chisiamo') }}">Chi siamo</a>
+            </li>
+            
+            <li class="nav-but ">
+                <a class="btn btn-outline-light" aria-current="page" href="{{ route('register') }}">Registrati</a>
+            </li>
+        
+           
+            <li class="nav-item">
+                <a class="btn btn-outline-light" href="{{ route('login') }}">Accedi</a>
+            </li>
+            @else
+            
+            <li class="nav-item">
+                <a class="nav-link " href="{{ route('announcement.create') }}">crea annuncio</a>
+            </li>
+            
+            <li class="nav-item mx-2">
+                <a class="btn btn-outline-light dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                    data-bs-toggle="dropdown" aria-expanded="false">
+                    {{ Auth::user()->name }}
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    {{-- <li><a class="dropdown-item" href="{{ route('announcement.create') }}">crea annuncio</a></li> --}}
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    
+                    <form id="form-logout" action="{{ route('logout') }}" method="post" class="d-none">
+                        @csrf
+                    </form>
+                </ul>
+
+            </li>
+            <li class="nav-item"><a class="btn btn-outline-light" href="/logout"
+                onclick="event.preventDefault();
+   getElementById('form-logout').submit()">logout</a>
+            </li>
+            
+            @endguest
+        </ul>
+
+
         </div>
     </div>
-</nav> 
+</nav>
