2929af312db0d0bd038a75af27ce9fb23a2e26f9
And1298 <andrealanciano@me.com>
2023-05-13 13:13:12 +0200

    aggiunto icone dove non visibili

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -15,25 +15,23 @@
 </div> --}}
 
 <div class="col col-3">
-    <div class=" pro">
+    <div class="pro">
         <img src="https://picsum.photos/200" alt="">
         <div class="des">
-            <span>{{ $announcement->created_at->format('d/m/y') }}</span>
-            <h5>{{ $announcement->title }}</h5>
-            <a href="#">{{ $announcement->category->name }}</a>
-            <div class="star">
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-            </div>
-            <h4>${{ $announcement->price }}</h4>
+          <h5 class="fs-4">{{ $announcement->title }}</h5>
+          <p>{{ $announcement->category->name }}</p class="lead">
+          <span>{{ $announcement->created_at->format('d/m/y') }}</span>
+          <div class="star">
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+              <i class="fa-solid fa-star"></i>
+          </div>
+          <h4>${{ $announcement->price }}</h4>
 
 
         </div>
-        <a href="#"><i class="fa-sharp fa-light fa-eye cart"></i></a>
-        <a href="{{ route('announcement.show', ['announcement' => $announcement]) }}"><i
-                class="fa-sharp fa-light fa-eye eye"></i></a>
+        <a href="{{ route('announcement.show', ['announcement' => $announcement]) }}"><i class="fa-solid fa-eye eye text-center"></i></a>
     </div>
 </div>
