21378fb4203a997b62cb8a35ff7889713ca175fa
And1298 <andrealanciano@me.com>
2023-05-16 21:22:05 +0200

    bug fissato

--- resources/views/components/_locale.blade.php
+++ resources/views/components/_locale.blade.php
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-<form class="d-inline" action="{{route('setLocale', $lang)}}" method="post">
-    @csrf
-    <button type="submit" class="btn">
-        <img src="{{asset('vendor/blade-flags/language-'.$lang.'.svg')}}" width="32" height="32" >
-    </button>
-    
-    </form>
-=======
 <form action="{{ route('setLocale', $lang) }}" method="POST">
     @csrf
     <button type="submit" class="btn">
         <img src="{{ asset('vendor/blade-flag/language-'.$lang.'.svg') }}" width="32px" height="32px" alt="">
     </button>
 </form>
->>>>>>> 5ceb3742ce5bebf65692e23993a7e0c4ca239276
