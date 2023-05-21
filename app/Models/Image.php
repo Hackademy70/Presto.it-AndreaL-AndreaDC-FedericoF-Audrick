48bc829e5b9c7b5f38a3ce486a0e0b599bb36a7a
And1298 <andrealanciano@me.com>
2023-05-21 11:10:01 +0200

    aggiustato bug

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -13,23 +13,6 @@ class Image extends Model
 
     protected $fillable =['path'];
 
-    public static function getUrlByFilePath($filePath , $w = null, $h= null){
-        if(!$w && !$h){
-            return Storage::url($filePath);
-        }
-        $path = dirname($filePath);
-        $filename = basename($filePath);
-        $file = "{$path}/crop_{$w}x{$h}__{$filename}";
-
-        return Storage::url($file);
-    }
-
-    public function getUrl($w = null, $h= null){
-        return Image::getUrlByFilePath($this->path, $w ,$h);
-    }
-
-
-
     protected $casts = [
         'labels' => 'array'
     ];
