fabec6e51b21633e4cda2dede37b568aae5f2f6e
Federico Ferrante <federico.ferrante98@gmail.com>
2023-05-22 16:56:44 +0200

    aggiunte traduzioni eng & es

--- resources/lang/en/ui.php
+++ resources/lang/en/ui.php
@@ -1,6 +1,42 @@
 <?php
-return[
-    'allAnnouncements'=>'Here\'s all our announcements',
-    'listaAnnunci'=>'Announcement list',
+return [
+    'allAnnouncements' => 'Here\'s all our announcements',
+    'listaAnnunci' => 'Announcement list',
+    'landimageTitle' => "It's time to free your wardrobe!",
+    'buttonShowMore' => "Learn more",
+    // navbar
+    'buttonSearch' => "Search",
+    'navbarAboutUs' => "About Us",
+    'navbarLogin' => "Login",
+    'navbarRegister' => "Register",
+    'navbarRevisor' => "Revisor",
+    'navbarRevisorInfo' => "Unread Messages",
+    // login page
+    'loginTitle' => 'Enter your credentials to log in',
+    'loginSecure' => "We won't share your data with anyone.",
+    'loginEmailLabel' => 'Email Address',
+    'loginPassLabel' => 'Password',
+    'loginButton' => 'Log in',
+    // register page
+    'registerTitle' => 'Enter your information to register',
+    'registerNameLabel' => 'Username',
+    'registerNamePlaceholder' => 'Giacomo1990',
+    'registerEmailLabel' => 'Email Address',
+    'registerEmailPlaceholder' => 'example@test.com',
+    'registerSecure' => "We won't share your data with anyone.",
+    'registerPassLabel' => 'Password',
+    'registerPassPlaceholder' => 'Password1?',
+    'registerPassInfo' => 'The password must meet the following requirements:',
+    'registerFirstInfo' => 'At least 8 characters',
+    'registerSecondInfo' => 'At least 1 special character from the following',
+    'registerThirdInfo' => 'At least 1 number',
+    'registerPassConfLabel' => 'Confirm Password',
+    'registerButton' => 'Register',
+    // Announcement Search Page -> announcement.index
+    'searchTitle' => 'The following announcements were found:',
+    'searchNotFound' => 'No announcements match the search criteria.',
+    // Create Article Page
+    'createTitleLabel' => 'Article Title',
+    'createImageLabel' => 'Upload up to a maximum of 5 images.'
 ];
 ?>
